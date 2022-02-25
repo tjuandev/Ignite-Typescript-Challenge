8f6b1e70ddf573800fd018cdebea710b35d66be2
ThiagoJuanDEV <tpydeveloper@gmail.com>
2022-02-24 22:08:57 -0300

    first commit

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef, useState, useCallback } from "react";
+
+import { useField } from "@unform/core";
+
+import { Container } from "./styles";
+
+interface IconProps {
+  size: number;
+}
+
+interface InputProps {
+  name: string;
+  icon?: React.FC<IconProps>;
+  [x: string]: any;
+}
+
+type BooleanStates = boolean;
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState<BooleanStates>(false);
+  const [isFilled, setIsFilled] = useState<BooleanStates>(false);
+
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: "value",
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
